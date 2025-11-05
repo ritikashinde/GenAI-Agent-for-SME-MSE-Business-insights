194a49089550d811cda5af2f6be85488a0aeccea
Ritika Shinde <156181898+ritikashinde@users.noreply.github.com>
2025-11-05 23:39:42 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def load_business_data(path="data/business_data.csv"):
+def load_business_data(path="business_data.csv"):
     df = pd.read_csv(path)
     df.fillna("", inplace=True)
     return df
